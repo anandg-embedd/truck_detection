3c5ac7487d6228f0a1da40191a4b430f5e276263
Anand Gopalakrishnan <agopalakrishnan@grundfos.com>
2024-03-06 07:59:19 +0530

    Updated code

--- predict.py
+++ predict.py
@@ -2,22 +2,35 @@ from keras.models import load_model
 
 import matplotlib.pyplot as plt
 
-piCamera = False
+picamera = False
 if picamera == True:
-    from picamera import PiCamera
+    #from picamera import PiCamera
     import time
-    from picamera.array import PiRGBArray
+    #from picamera.array import PiRGBArray
+    import cv2
+    webcam = cv2.VideoCapture(0)
+    time.sleep(2)
 
 import argparse
 import script.utils
+import numpy as np
+
+
 
 def parse_args():
     desc = "Vehicle Classification"
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('--model', type=str, default='model/vehicle_model_saved.h5', help='Where Is Model File?')
-    parser.add_argument('--img', type=str, default='data/1.jpg', help='What Is Images Path?')
+    parser.add_argument('--model', type=str, default='model/categorical_vehicle_model_saved.h5', help='Where Is Model File?')
+    parser.add_argument('--img', type=str, default='dataset/train/Toyota Innova/94.jpg', help='What Is Images Path?')
 
     return parser.parse_args()
+
+def img_pre_process(img):
+    img_width, img_height = 224, 224
+    #img      = img.astype(np.float32)
+    img      = img.resize(img_width,img_height) #cropping the image
+    img = img/255.0
+    return img
 
 def main():
     args = parse_args()
@@ -30,13 +43,24 @@ def main():
             model = load_model(args.model)
             # Convert Image To Numpy Array
             if picamera == True:
-                camera = PiCamera()
-                rawImage = PiRGBArray(camera)
-                time.sleep(0.1)
-                camera.capture(rawImage, format = "rgb")
-                image = rawImage.array
+##                camera = PiCamera()
+##                rawImage = PiRGBArray(camera)
+##                time.sleep(0.1)
+##                camera.capture(rawImage, format = "rgb")
+##                image = rawImage.array
+                try:
+                    check, frame = webcam.read()
+                    image = img_pre_process(frame)
+                except(KeyboardInterrupt):
+                    print("Turning off camera.")
+                    webcam.release()
+                    print("Camera off.")
+                    print("Program ended.")
+                    cv2.destroyAllWindows()
+                    break
             else:
                 image = script.utils.load_image(args.img)
+            #image = img_pre_process(image)
             # Predict Image Based On Model
             label = model.predict(image)
             # Print Result
--- train.py
+++ train.py
@@ -1,24 +1,21 @@
 # Importing all necessary libraries
-import os
-os.environ["KERAS_BACKEND"] = "torch"
-
-import keras_core as keras
 
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Convolution2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers import BatchNormalization
 from keras import backend as K
  
 img_width, img_height = 224, 224
 
 train_data_dir = 'dataset/train'
 validation_data_dir = 'dataset/test'
-nb_train_samples = 400
-nb_validation_samples = 100
-epochs = 10
+nb_train_samples = None
+nb_validation_samples = 4
+epochs = 15
 batch_size = 16
-classes_number = 2 # Cars and Planes
+classes_number = 7 # Cars and Planes
 
 def main():
     if K.image_data_format() == 'channels_first':
@@ -26,26 +23,64 @@ def main():
     else:
         input_shape = (img_width, img_height, 3)
 
+    # Initializing the CNN
     model = Sequential()
-    model.add(Conv2D(32, (2, 2), input_shape=input_shape))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    
-    model.add(Conv2D(32, (2, 2)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    
-    model.add(Conv2D(64, (2, 2)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    
+    # Convolution Step 1
+    model.add(Convolution2D(96, 11, strides = (4, 4), padding = 'valid', input_shape=input_shape, activation = 'relu'))
+    # Max Pooling Step 1
+    model.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2), padding = 'valid'))
+    model.add(BatchNormalization())
+    # Convolution Step 2
+    model.add(Convolution2D(256, 11, strides = (1, 1), padding='valid', activation = 'relu'))
+    # Max Pooling Step 2
+    model.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2), padding='valid'))
+    model.add(BatchNormalization())
+    # Convolution Step 3
+    model.add(Convolution2D(384, 3, strides = (1, 1), padding='valid', activation = 'relu'))
+    model.add(BatchNormalization())
+    # Convolution Step 4
+    model.add(Convolution2D(384, 3, strides = (1, 1), padding='valid', activation = 'relu'))
+    model.add(BatchNormalization())
+    # Convolution Step 5
+    model.add(Convolution2D(256, 3, strides=(1,1), padding='valid', activation = 'relu'))
+    # Max Pooling Step 3
+    model.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2), padding = 'valid'))
+    model.add(BatchNormalization())
+    # Flattening Step
     model.add(Flatten())
-    model.add(Dense(64))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    # 1 For binary classification and CLASS_NUMBER for categorical classification
-    model.add(Dense(classes_number))
-    model.add(Activation('sigmoid'))
+    # Full Connection Step
+    model.add(Dense(units = 4096, activation = 'relu'))
+    model.add(Dropout(0.4))
+    model.add(BatchNormalization())
+    model.add(Dense(units = 4096, activation = 'relu'))
+    model.add(Dropout(0.4))
+    model.add(BatchNormalization())
+    model.add(Dense(units = 1000, activation = 'relu'))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+    model.add(Dense(units = classes_number, activation = 'softmax'))
+
+
+##    model = Sequential()
+##    model.add(Conv2D(32, (2, 2), input_shape=input_shape))
+##    model.add(Activation('relu'))
+##    model.add(MaxPooling2D(pool_size=(2, 2)))
+##    
+##    model.add(Conv2D(32, (2, 2)))
+##    model.add(Activation('relu'))
+##    model.add(MaxPooling2D(pool_size=(2, 2)))
+##    
+##    model.add(Conv2D(64, (2, 2)))
+##    model.add(Activation('relu'))
+##    model.add(MaxPooling2D(pool_size=(2, 2)))
+##    
+##    model.add(Flatten())
+##    model.add(Dense(64))
+##    model.add(Activation('relu'))
+##    model.add(Dropout(0.5))
+##    # 1 For binary classification and CLASS_NUMBER for categorical classification
+##    model.add(Dense(classes_number))
+##    model.add(Activation('sigmoid'))
 
     # It can be 'categorical_crossentropy' or 'binary_crossentropy'
     model.compile(loss='categorical_crossentropy',optimizer='rmsprop',metrics=['accuracy'])
@@ -68,10 +103,12 @@ def main():
     
     model.fit_generator(
         train_generator,
-        steps_per_epoch=nb_train_samples // batch_size,
+        steps_per_epoch=nb_train_samples, # batch_size,
         epochs=epochs,
         validation_data=validation_generator,
-        validation_steps=nb_validation_samples // batch_size
+        validation_steps=nb_validation_samples, # batch_size
+        verbose=1,
+        shuffle=True
     )
 
     model.save('model\categorical_vehicle_model_saved.h5')
